f3a2fc0eaa299f03f0a4707556ae95bfc035c5d6
Trang6998 <tranghc8a@gmail.com>
2019-05-30 08:46:15 +0700

    commit

---
+++ Source/WEBBANHANG/WEBBANHANG/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WEBBANHANG
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
